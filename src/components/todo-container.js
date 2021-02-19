08b3825c83884cbaf7c124c785450767044617bd
zedr <zedr@zedr.com>
2021-02-19 21:39:59 +0100

    Use props

--- src/components/todo-container.js
+++ src/components/todo-container.js
@@ -1,19 +1,32 @@
 import React from "react";
 
-class TodoItem extends React.Component {
-  render() {
-    return (
-      <li>Hi!</li>
-    )
-  }
+function TodoItem(props) {
+  return (
+    <li>
+    <input type="checkbox" 
+           checked={props.todo.ok}
+           onChange={props.handleChangeProps}
+      />{props.todo.title}
+    </li>
+  )
 }
 
 class TodoContainer extends React.Component {
 
+  state = {
+    items: [{id: 1, title: "hello", ok: true}, {id: 2, title: "moto", ok: false}]
+  }
+
+  handleChange() {
+    console.log('Click');
+  }
+
   render() {
     return (
       <ul>
-        <TodoItem />
+        {this.state.items.map(item => (
+          <TodoItem key={item.id} todo={item} handleChangeProps={this.handleChange} />
+        ))}
       </ul>
     )
   }
