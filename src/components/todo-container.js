770b379d5bab5fcc50dd40f3f7ef5b5f480f8214
zedr <zedr@zedr.com>
2021-02-21 19:14:24 +0100

    Process the form as JSON

--- src/components/todo-container.js
+++ src/components/todo-container.js
@@ -1,33 +1,59 @@
 import React from "react";
 
-function TodoItem(props) {
-  return (
-    <li>
-    <input type="checkbox" 
-           checked={props.todo.ok}
-           onChange={props.handleChangeProps}
-      />{props.todo.title}
-    </li>
-  )
+const API_URL = "http://localhost:3000";
+
+class TodoItem extends React.Component {
+
+    handleSubmit(ev) {
+        ev.preventDefault();
+        const json = {"form": ev.target.id, "data": {}};
+        new FormData(ev.target).forEach(function(value, prop){
+          json.data[prop] = value
+        });
+        console.log(JSON.stringify(json));
+        fetch(
+            API_URL,
+            {
+                method: "POST",
+                headers: {},
+                body: JSON.stringify(json)
+            }
+        ).then((resp) => console.log(resp));
+    }
+
+    render() {
+      const item = this.props.item;
+      return (
+        <div>
+          <label>{item.shopName}</label>
+          <form id={item.shopName} onSubmit={this.handleSubmit}>
+            <label>First name:</label>
+            <input type="text" id="fname" name="fname" />
+            <label>Last name:</label>
+            <input type="text" id="lname" name="lname" />
+            <button type="submit" value="Submit">Submit</button>
+          </form>
+        </div>
+      )
+    }
 }
 
 class TodoContainer extends React.Component {
 
   state = {
-    items: [{id: 1, title: "hello", ok: true}, {id: 2, title: "moto", ok: false}]
-  }
-
-  handleChange() {
-    console.log('Click');
+    items: [
+        {shopName: "Shop1"},
+        {shopName: "Shop2"}
+    ]
   }
 
   render() {
     return (
-      <ul>
-        {this.state.items.map(item => (
-          <TodoItem key={item.id} todo={item} handleChangeProps={this.handleChange} />
+      <div>
+        {this.state.items.map((item, index) => (
+          <TodoItem key={index} item={item} />
         ))}
-      </ul>
+      </div>
     )
   }
 
