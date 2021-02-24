a282a7de7d39ffc79fa1d4c228c1247ace49ce90
zedr <zedr@zedr.com>
2021-02-24 20:29:00 +0100

    Convert to functions

--- src/components/todo-container.js
+++ src/components/todo-container.js
@@ -2,59 +2,49 @@ import React from "react";
 
 const API_URL = "http://localhost:3000";
 
-class TodoItem extends React.Component {
-
-    handleSubmit(ev) {
-        ev.preventDefault();
-        const row = [new Date().toLocaleString(), ev.target.id];
-        new FormData(ev.target).forEach((value) => row.push(value));
-        console.log(JSON.stringify(row));
-        fetch(
-            API_URL,
-            {
-                method: "POST",
-                headers: {},
-                body: JSON.stringify(row)
-            }
-        ).then((resp) => console.log(resp));
-    }
-
-    render() {
-      const item = this.props.item;
-      return (
-        <div>
-          <label>{item.shopName}</label>
-          <form id={item.shopName} onSubmit={this.handleSubmit}>
-            <label>First name:</label>
-            <input type="text" id="fname" name="fname" />
-            <label>Last name:</label>
-            <input type="text" id="lname" name="lname" />
-            <button type="submit" value="Submit">Submit</button>
-          </form>
-        </div>
-      )
-    }
+const TodoItem = (props) => {
+  function handleSubmit(ev) {
+      ev.preventDefault();
+      const row = [new Date().toLocaleString(), ev.target.id];
+      new FormData(ev.target).forEach((value) => row.push(value));
+      console.log(JSON.stringify(row));
+      fetch(
+          API_URL,
+          {
+              method: "POST",
+              headers: {},
+              body: JSON.stringify(row)
+          }
+      ).then((resp) => console.log(resp));
+  }
+  return (
+    <div>
+      <label>{props.item.shopName}</label>
+      <form id={props.item.shopName} onSubmit={handleSubmit}>
+        <label>First name:</label>
+        <input type="text" id="fname" name="fname" />
+        <label>Last name:</label>
+        <input type="text" id="lname" name="lname" />
+        <button type="submit" value="Submit">Submit</button>
+      </form>
+    </div>
+  )
 }
 
-class TodoContainer extends React.Component {
-
-  state = {
+const TodoContainer = () => {
+  const state = {
     items: [
         {shopName: "Shop1"},
         {shopName: "Shop2"}
     ]
   }
-
-  render() {
-    return (
-      <div>
-        {this.state.items.map((item, index) => (
-          <TodoItem key={index} item={item} />
-        ))}
-      </div>
-    )
-  }
-
+  return (
+    <div>
+      {state.items.map((item, index) => (
+        <TodoItem key={index} item={item} />
+      ))}
+    </div>
+  )
 }
 
 export default TodoContainer;
